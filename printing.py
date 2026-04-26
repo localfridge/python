13d28e1045c13d4e15df65c2561582bad6f8d72c
localfridge <klokowzaen@gmail.com>
2026-04-26 18:28:59 +0200

    printing 26 april py

---
+++ printing.py
@@ -0,0 +1,9 @@
+#is a comment
+#shows words
+print("hello world")
+
+print("beliefs change reality not reality changes beliefs")
+print("i love spiders")
+
+print("things are almost never like they seem")
+print("smooth like butter")
