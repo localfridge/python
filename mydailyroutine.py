7d9fad59fe6d0335c8ca7bb22897e45ec9239cc0
localfridge <klokowzaen@gmail.com>
2026-04-27 19:14:26 +0200

    mydailyroutine

---
+++ mydailyroutine.py
@@ -0,0 +1,15 @@
+#how my skoolweeks daily routine goes
+
+print("wake up get dressed and brush teeth")
+
+print("work and pay attention at school")
+
+print("do homework in breaks")
+
+print("go home")
+
+print("listen to music and rest a bit")
+
+print("do leftover homework or study if able")
+
+print("go to sleep hopefully at a reasonable hour")
