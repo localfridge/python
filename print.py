b2c93df02ccbc1ef9d9945f92f07d30f61a809e2
localfridge <klokowzaen@gmail.com>
2026-05-02 18:22:22 +0200

    printing2

---
+++ print.py
@@ -0,0 +1,15 @@
+#print a sentence
+print("hey how was your day")
+
+#print a number
+print(9)
+
+#break line to go below
+print("mine was good \n but i woke up too early")
+
+#how to add spaces you do it by adding commas between strings and numbers
+#how to write strings and numbers
+print("i woke up at",6,"am")
+
+
+
