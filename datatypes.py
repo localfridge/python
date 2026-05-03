4e05185cc296a1a646dad8c99b72c17fb8eabe10
localfridge <klokowzaen@gmail.com>
2026-05-03 18:12:55 +0200

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+#type is already programmed in py and is used to say wich datatypes it is
+#it is py so no need of brackets by the variables
+a=1
+print(type(a))
+
+b="war never changes"
+print(type(b))
+
+c=2.5
+print(type(c))
+
+d=True
+print(type(d))
