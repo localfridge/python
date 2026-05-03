bd325116e16222a0962dfad634b72a9595f2fe0e
localfridge <klokowzaen@gmail.com>
2026-05-03 18:38:49 +0200

    typecast

---
+++ typecasting.py
@@ -0,0 +1,12 @@
+d=6
+#have to say its type first and print it then you can change the datatype so that it can be printed alongside others
+#change datatype before printing
+d=str(d)
+
+print("number",type(d))
+
+c="blobby"
+
+print(c+d)
+
+
