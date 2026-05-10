ed498cefe5dffe90f15276b257209987162b394d
localfridge <klokowzaen@gmail.com>
2026-05-10 18:36:58 +0200

    progitloss

---
+++ profitloss.py
@@ -0,0 +1,16 @@
+ 
+
+
+buyingprice=float (input("Enter the buying price of the item:"))
+sellingprice=float (input("Enter the selling price of the item::"))
+
+if (sellingprice>buyingprice):
+    profit=sellingprice-buyingprice
+    print("you have made a profit")
+    
+
+
+else:
+    loss=buyingprice-sellingprice
+    print("you have made a loss")
+    
