58aa9c7294f20f4fd8df53cb96d49888b34f85ab
localfridge <klokowzaen@gmail.com>
2026-05-10 18:49:20 +0200

    greaterorsmaller

---
+++ greaterorsmaller.py
@@ -0,0 +1,8 @@
+num=int(input("enter any number:"))
+
+if (num>15):
+    print(num,"your number is greater than 15")
+else:
+    print(num,"your number is lesser than 15")
+
+print("this is outside the if-else block")
