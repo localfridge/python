a8c763423d22707aca36065bf2a1b3987f612e55
localfridge <klokowzaen@gmail.com>
2026-05-16 18:40:06 +0200

    andoroperators

---
+++ andoroperator.py
@@ -0,0 +1,22 @@
+a=4
+b=7
+c=0
+
+
+if a and b and c:
+    print("these boolean values are equal to true")
+else:
+     print("these boolean values are equal to false")
+
+
+if a>0 and b>6:
+      print("either of the number values are greaterthan the original")
+else:
+     print("either of the number values are lesserthan the original")
+
+if a>10 and b>36:
+      print("either of the number values are greaterthan the original")
+else:
+     print("either of the number values are lesserthan the original")
+
+
