319a6d60f72c0f2bddf4e793f318dc2b137c37c7
localfridge <klokowzaen@gmail.com>
2026-05-16 19:04:50 +0200

    notoperatorsinpython16

---
+++ notoperatorinpythn.py
@@ -0,0 +1,19 @@
+a=10
+b=43
+c=8
+
+if not(a==b):
+    print("true")
+
+if (a==b):
+    print("false")
+
+if not (a==b) and (a==10):
+    print("michael")
+
+r="python"
+u="hampter"
+
+if not(r==u):
+    print(a,"and",b,"are not equal")
+
