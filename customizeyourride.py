4ed5a4a8269e5fb2342e03ca4c1718af10b0a029
localfridge <klokowzaen@gmail.com>
2026-05-17 18:53:59 +0200

    cusromizeyourride

---
+++ customizeyourride.py
@@ -0,0 +1,33 @@
+print("1.car")
+print("2.scooter")
+
+choice=int(input("Enter your choice:"))
+
+if  choice==2:
+    print("1.scooty")
+    print("2.scooter\n")
+    choose=int(input("input you choice"))
+    if choose==2:
+        print("you have selected the scooter")
+    else:
+                print("you have selected the scooter")
+
+elif choice==1:
+       print("what type of car do you want?")
+       print("1.honda")
+       print("2.nissan")
+
+       car=int(input("input you choice"))
+
+       if car==1:
+              print("you have chosen honda")
+       else:
+              print("you have chosen nissan")
+else:
+    print("invalid")
+
+
+
+
+
+
